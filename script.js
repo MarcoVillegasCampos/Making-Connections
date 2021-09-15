1248b3109878d2764e460c24e9c22999ce03d415
Your Name <you@example.com>
2021-09-14 23:22:00 -0600

    adding files

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 console.log("page loaded...");
+
+
+function changeName(event){
+    console.log(event.target);
+    let variable= document.querySelector('#name');
+    variable.innerHTML= "Marco Villegas C";
+
+
+
+    
+}
+
+function remove (event){
+    
+    
+    let variable= event.target.closest('.card-list-item');
+    variable.remove();
+
+}
+
+   
+    
+var spans = [document.querySelector('#requests')];
+var likes = [2];
+    function like(id) {
+ 
+    likes[id]--;
+    spans[id].innerHTML= likes[id];
+    }
+
+    
+    
+    var spans2 = [document.querySelector('#connections')];
+    var connection = [500];
+    function connections(id) {
+    
+   
+    connection[id]++;
+    spans2[id].innerHTML= connection[id]+"+";
+    }
+    
+
+
+
